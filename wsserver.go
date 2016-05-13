26b05b5dd6f268c61d3c40a3de9b7bac09129df9
Centimitr <Centimitr@Gmail.com>
2016-05-13 18:26:56 +0800

    update

---
+++ wsserver.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+    "net/http"
+    "golang.org/x/net/websocket"
+    "encoding/json"
+)
+type Req struct{
+	Id string `json:'id'`
+	Method string `json:'method'`
+	Params string `json:'params'`
+	Data string `json:'data'`
+}
+// Echo the data received on the WebSocket.
+func Server(ws *websocket.Conn) {
+    var err error
+
+    for {
+        var req string
+        if err = websocket.Message.Receive(ws, &req); err != nil {
+            fmt.Println("WAITING.")
+            break
+        }
+        var r Req
+        json.Unmarshal([]byte(req),&r)
+        fmt.Println("RECEIVED.")
+        res := `
+        {
+    		"id":"`+r.Id+`",
+    		"params":{},
+    		"data":{
+    			"articles":[]
+    		},
+    		"error":{}
+    	}`
+        fmt.Println("SEND.")
+
+        if err = websocket.Message.Send(ws, res); err != nil {
+            fmt.Println("SEND ERROR.")
+            break
+        }
+    }
+}
+
+func main() {
+    http.Handle("/echo", websocket.Handler(Server))
+    err := http.ListenAndServe(":12345", nil)
+    if err != nil {
+        panic("ListenAndServe: " + err.Error())
+    }
+}
