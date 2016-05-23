cb390c2ce925776940a890766383e65692a2e467
Centimitr <Centimitr@gmail.com>
2016-05-23 17:51:11 +0800

    update

--- server/xstatistics/middleware.go
+++ server/xstatistics/middleware.go
@@ -2,6 +2,7 @@ package xstatistics
 
 import (
 	msg "github.com/Centimitr/xmessage"
+	"time"
 )
 
 /*
@@ -10,6 +11,7 @@ import (
 
 func (s StatisticsMap) AfterReceive(req *msg.Req) {
 	s.addRequest(req.Method)
+	s.timeMap[req.Id] = time.Now()
 }
 
 func (s StatisticsMap) BeforeProcess(ctx *msg.Ctx) {
@@ -24,6 +26,30 @@ func (s StatisticsMap) BeforeSend(res *msg.Res) {
 
 func (s StatisticsMap) AfterSend(res *msg.Res) {
 	s.addResponse(res.Method)
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	duration := time.Now().Sub(s.timeMap[res.Id]).Seconds()
+	if s.methodMap[res.Method].MinDuration < 1e-6 {
+		s.methodMap[res.Method].MinDuration = duration
+	}
+	switch {
+	case s.methodMap[res.Method].MinDuration-duration > 1e-6:
+		s.methodMap[res.Method].MinDuration = duration
+	case duration-s.methodMap[res.Method].MaxDuration > 1e-6:
+		s.methodMap[res.Method].MaxDuration = duration
+	case true:
+		times := s.methodMap[res.Method].ResponseTimes
+		s.methodMap[res.Method].AvgDuration = (float64(times-1)*s.methodMap[res.Method].AvgDuration + duration) / float64(times)
+	}
+	delete(s.timeMap, res.Id)
+	if len(s.timeMap) > 64000 {
+		for id, t := range s.timeMap {
+			d := time.Now().Sub(t)
+			if d > time.Second {
+				delete(s.timeMap, id)
+			}
+		}
+	}
 }
 
 /*
@@ -39,6 +65,7 @@ func init() {
 	msg.LoadMiddleware(Statistics{
 		StatisticsMap{
 			methodMap: make(map[string]*StatisticsItem),
+			timeMap:   make(map[string]time.Time),
 		},
 	})
 }
--- server/xstatistics/statistics.go
+++ server/xstatistics/statistics.go
@@ -3,17 +3,22 @@ package xstatistics
 import (
 	"fmt"
 	"sync"
+	"time"
 )
 
 type StatisticsItem struct {
 	// Method        string
 	RequestTimes  int
 	ResponseTimes int
+	AvgDuration   float64
+	MinDuration   float64
+	MaxDuration   float64
 }
 
 type StatisticsMap struct {
 	lock      sync.RWMutex
 	methodMap map[string]*StatisticsItem
+	timeMap   map[string]time.Time
 }
 
 /*
@@ -50,7 +55,8 @@ func (s *StatisticsMap) addResponse(method string) {
 func (s *StatisticsMap) get() {
 	s.lock.RLock()
 	defer s.lock.RUnlock()
+	fmt.Printf("\n %-35s %-10s %-10s %-10s %-10s %-10s\n", "API", "ReqTimes", "ResTimes", "Avg", "Min", "Max")
 	for k, item := range s.methodMap {
-		fmt.Println(k, "ReqTimes:", item.RequestTimes, "ResTimes:", item.ResponseTimes)
+		fmt.Printf(" %-35s %-10v %-10v %-10f %-10f %-10f\n", k, item.RequestTimes, item.ResponseTimes, item.AvgDuration*1000, item.MinDuration*1000, item.MaxDuration*1000)
 	}
 }
